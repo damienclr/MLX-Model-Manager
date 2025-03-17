afe689f3cfccdd42878b872e142ee677f1242bad
Damien <131157132+damienclr@users.noreply.github.com>
2025-03-17 11:57:36 +0100

    Update ModelFactory.swift

--- Sources/MLXModelManager/ModelFactory.swift
+++ Sources/MLXModelManager/ModelFactory.swift
@@ -60,7 +60,7 @@ public class ModelTypeRegistry: @unchecked Sendable {
         "phi3": create(Phi3Configuration.self, Phi3Model.init),
         "phimoe": create(PhiMoEConfiguration.self, PhiMoEModel.init),
         "gemma": create(GemmaConfiguration.self, GemmaModel.init),
-        "gemma3": create(Gemma3Configuration.self, Gemma3Model.init),
+        "gemma2": create(Gemma2Configuration.self, Gemma2Model.init),
         "qwen2": create(Qwen2Configuration.self, Qwen2Model.init),
         "starcoder2": create(Starcoder2Configuration.self, Starcoder2Model.init),
         "cohere": create(CohereConfiguration.self, CohereModel.init),
@@ -202,8 +202,20 @@ public class ModelRegistry: @unchecked Sendable {
         prompt in "<|user|>\n\(prompt)<|end|>\n<|assistant|>\n"
     }
 
-    static public let gemma_3_1b_it_8bit = ModelConfiguration(
-        id: "mlx-community/gemma-3-1b-it-8bit",
+    static public let gemma2bQuantized = ModelConfiguration(
+        id: "mlx-community/quantized-gemma-2b-it",
+        overrideTokenizer: "PreTrainedTokenizer",
+        defaultPrompt: "what is the difference between lettuce and cabbage?"
+    )
+
+    static public let gemma_2_9b_it_4bit = ModelConfiguration(
+        id: "mlx-community/gemma-2-9b-it-4bit",
+        overrideTokenizer: "PreTrainedTokenizer",
+        defaultPrompt: "What is the difference between lettuce and cabbage?"
+    )
+
+    static public let gemma_2_2b_it_4bit = ModelConfiguration(
+        id: "mlx-community/gemma-2-2b-it-4bit",
         overrideTokenizer: "PreTrainedTokenizer",
         defaultPrompt: "What is the difference between lettuce and cabbage?"
     )
@@ -258,7 +270,9 @@ public class ModelRegistry: @unchecked Sendable {
     private static func all() -> [ModelConfiguration] {
         [
             codeLlama13b4bit,
-            gemma_3_1b_it_8bit,
+            gemma2bQuantized,
+            gemma_2_2b_it_4bit,
+            gemma_2_9b_it_4bit,
             llama3_1_8B_4bit,
             llama3_2_1B_4bit,
             llama3_2_3B_4bit,
@@ -422,4 +436,5 @@ extension ModelConfiguration.Identifier {
         }
     }
 }
+
 
